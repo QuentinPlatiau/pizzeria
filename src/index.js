c36cf4eb1746b960ad772053a5071ae8115afda2
e-baron <raphael.baroni@vinci.be>
2022-07-08 13:52:14 +0200

    fix : prettier more compliant to Airbnb style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-// Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Custom styles
 import './stylesheets/main.css';
-
-// This is the entry point to your app : add all relevant import and custom code
